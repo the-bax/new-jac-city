42ed6e79bfc50813149db5f3f0edadf5089c0d4e
Nate Bo Wang <nathanbotinker@gmail.com>
2021-03-11 14:53:40 +1300

    feat(contentful-demo-page): fetch article at build time
    Create a GraphQL query to select the article by title.
    Post the query to the Contentful GraphQL Content API.
    Get the article at build time by Next's getStaticProps method.
    Display the article on the static page.

--- pages/demo/contentful.tsx
+++ pages/demo/contentful.tsx
@@ -1,3 +1,24 @@
-export default function Contentful(): JSX.Element {
-  return <div>{'Some content from Contentful'}</div>
+import { GetStaticProps } from 'next'
+import getArticleByTitle, { Article } from './utilities/getArticleByTitle'
+
+export type ContentfulProps = {
+  article: Article
+}
+
+export default function Contentful({ article }: ContentfulProps): JSX.Element {
+  return (
+    <>
+      <div>{article.title}</div>
+      <div>{article.content}</div>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const article = await getArticleByTitle('JS')
+  return {
+    props: {
+      article,
+    },
+  }
 }
---
+++ pages/demo/utilities/getArticleByTitle.ts
@@ -0,0 +1,32 @@
+export type Article = {
+  title: string
+  content: string
+}
+
+export default async function getArticleByTitle(title: string): Promise<Article> {
+  const query = `
+    {
+      articleCollection(where: {title: "${title}"}) {
+        items {
+          title
+          content
+        }
+      }
+    }
+  `
+  const res = await fetch(`https://graphql.contentful.com/content/v1/spaces/zqvr29h6rh51/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer qVGC4H5ZmObAQMKyniPJNtNjtGkfmhi_OgVVy9jmtR8',
+    },
+    body: JSON.stringify({ query }),
+  })
+
+  const { data } = await res.json()
+  console.log(data.articleCollection)
+  // articleCollection can only contains zero or one article. Since title is an unique field.
+  const article: Article = data.articleCollection.items[0]
+
+  return article
+}
