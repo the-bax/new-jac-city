db5036a49be20ebf75d2d99a89344628d5f6585f
Nate Bo Wang <nathanbotinker@gmail.com>
2021-03-16 14:09:00 +1300

    feat(contentful-demo-page): add title for the test

--- components/Article/index.spec.tsx
+++ components/Article/index.spec.tsx
@@ -1,7 +1,7 @@
 import Article from './'
 import { render } from '@testing-library/react'
 
-test('', () => {
+test('should show title and content', () => {
   const { getByText } = render(<Article title="JS" content="JavaScript is a programming language." />)
   expect(getByText('JS'))
   expect(getByText('JavaScript is a programming language.'))
