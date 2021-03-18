15d914c758ce897f4e741967e48231ff8594e4c4
Nate Bo Wang <nathanbotinker@gmail.com>
2021-03-19 09:02:31 +1300

    feat(contentful-demo-page): add blank lines

--- components/Main/PageContent/index.spec.tsx
+++ components/Main/PageContent/index.spec.tsx
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react'
 
 test('should show title and content', () => {
   const { getByText } = render(<PageContent title="JS" content="JavaScript is a programming language." />)
+
   expect(getByText('JS'))
   expect(getByText('JavaScript is a programming language.'))
 })
--- utilities/constants.ts
+++ utilities/constants.ts
@@ -1,5 +1,6 @@
 export const CONTENTFUL_API_ACCESS_TOKEN = 'Bearer qVGC4H5ZmObAQMKyniPJNtNjtGkfmhi_OgVVy9jmtR8'
 export const CONTENTFUL_API_URL = 'https://graphql.contentful.com/content/v1/spaces/zqvr29h6rh51/'
+
 export enum HttpRequestMethod {
   GET = 'GET',
   POST = 'POST',
