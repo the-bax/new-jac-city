716eacbe396089151840e86a287981b7890ea7cb
Nate Bo Wang <nathanbotinker@gmail.com>
2021-04-07 10:07:27 +1200

    refactor: import ReactElement as type

--- test/test-utils.tsx
+++ test/test-utils.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react'
-import type { ReactNode } from 'react'
+import type { ReactElement, ReactNode } from 'react'
 import type { RenderOptions, RenderResult } from '@testing-library/react'
 
 export type ProvidersProps = {
@@ -10,7 +10,7 @@ const Providers = ({ children }: ProvidersProps): JSX.Element => {
   return <>{children}</>
 }
 
-const customRender = (ui: React.ReactElement, options: RenderOptions = {}): RenderResult =>
+const customRender = (ui: ReactElement, options: RenderOptions = {}): RenderResult =>
   render(ui, { wrapper: Providers, ...options })
 
 export * from '@testing-library/react'
