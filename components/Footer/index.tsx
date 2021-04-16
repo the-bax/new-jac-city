a0277053760f3f558ac27ee66e373e61e63ebc26
Nathan Wang <nathanbotinker@gmail.com>
2021-04-16 19:47:16 +1200

    feat(footer): add target and rel attributes

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -11,6 +11,8 @@ export default function Footer(): JSX.Element {
           aria-label="Go to Facebook page."
           className={utilityStyle.linkColorLight}
           href="https://www.facebook.com/TheJacNZ/"
+          rel="noreferrer"
+          target="_blank"
         >
           <Facebook aria-label="Facebook icon" className={style.icon} />
         </a>
@@ -18,7 +20,7 @@ export default function Footer(): JSX.Element {
       <span>Copyright 2021 The Jac</span>
       <span>
         Site built by&nbsp;
-        <a className={utilityStyle.linkColorLight} href="https://paperhat.ltd/">
+        <a className={utilityStyle.linkColorLight} href="https://paperhat.ltd/" rel="noreferrer" target="_blank">
           Paperhat
         </a>
       </span>
