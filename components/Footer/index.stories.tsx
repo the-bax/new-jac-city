20b507671f146a2870c7264f436a958b417f01fd
Nate Bo Wang <nathanbotinker@gmail.com>
2021-04-19 12:36:36 +1200

    refactor: rename css space vars, remove mock main component

--- components/Footer/index.stories.tsx
+++ components/Footer/index.stories.tsx
@@ -1,24 +1,8 @@
 import Footer from '.'
-import MockMain from './MockMain'
 import type { Meta } from '@storybook/react'
-import type { ReactElement } from 'react'
 
 const meta: Meta = {
   component: Footer,
-  decorators: [
-    (Story: () => ReactElement): JSX.Element => (
-      <>
-        <MockMain />
-        <Story />
-      </>
-    ),
-  ],
-  parameters: {
-    a11y: {
-      element: '#root *:not(main, main *)',
-    },
-    layout: 'fullscreen',
-  },
   title: 'components/Footer',
 }
 
