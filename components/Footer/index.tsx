ba964c223791e46a2192d9ac46f23b3351400f41
Nate Bo Wang <nathanbotinker@gmail.com>
2021-04-20 11:56:59 +1200

    refactor: replace color classes with css variables

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,26 +1,19 @@
 import style from './Footer.module.css'
-import utilityStyle from './utilities.module.css'
 import { SiFacebook as Facebook } from 'react-icons/si'
 
 export default function Footer(): JSX.Element {
   return (
-    <footer className={[style.footer, utilityStyle.bgCloudBurst, utilityStyle.colorLight].join(' ')}>
+    <footer className={style.footer}>
       <span className={style.withIcon}>
         Connect with us on&nbsp;
-        <a
-          aria-label="Go to Facebook page."
-          className={utilityStyle.linkColorLight}
-          href="https://www.facebook.com/TheJacNZ/"
-          rel="noreferrer"
-          target="_blank"
-        >
+        <a aria-label="Go to Facebook page." href="https://www.facebook.com/TheJacNZ/" rel="noreferrer" target="_blank">
           <Facebook aria-label="Facebook icon" className={style.icon} />
         </a>
       </span>
       <span>Copyright 2021 The Jac</span>
       <span>
         Site built by&nbsp;
-        <a className={utilityStyle.linkColorLight} href="https://paperhat.ltd/" rel="noreferrer" target="_blank">
+        <a href="https://paperhat.ltd/" rel="noreferrer" target="_blank">
           Paperhat
         </a>
       </span>
