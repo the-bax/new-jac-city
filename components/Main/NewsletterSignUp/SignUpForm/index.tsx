21fa2cc7b7c378d1e71390dcbd68b9ec961ce082
Nate Bo Wang <nathanbotinker@gmail.com>
2021-04-27 20:07:11 +1200

    feat(newsletter-sign-up): create newsletter sign up component

---
+++ components/Main/NewsletterSignUp/SignUpForm/index.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import type { ChangeEvent, Dispatch, FormEvent, SetStateAction } from 'react'
+
+export type SignUpFormProps = {
+  setIsSuccessful: Dispatch<SetStateAction<boolean>>
+}
+
+export default function SignUpForm({ setIsSuccessful }: SignUpFormProps): JSX.Element {
+  const [email, setEmail] = useState('')
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault()
+    setEmail(event.target.value)
+  }
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setIsSuccessful(true)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        aria-invalid="false"
+        aria-label="Enter your email address."
+        aria-required="true"
+        name="email"
+        onChange={handleChange}
+        placeholder="Enter Email"
+        required
+        type="email"
+        value={email}
+      />
+      <button type="submit">Join</button>
+    </form>
+  )
+}
